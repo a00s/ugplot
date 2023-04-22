2c0b18a0d95aa9e7cc40bbeec8beb799fa380209
Thiago <a00s@a00s.com>
2023-04-22 11:49:04 -0300

    transpose working

--- R/app.R
+++ R/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(ggplot2)
 library(heatmap3)
 library(DT)
+library(gplots)
 
 plotlist <- read.csv("plotlist.csv", sep=";", header = TRUE)
 print(plotlist)
@@ -14,56 +15,67 @@ ui <- fluidPage(
     }
   ")),
   titlePanel(tags$img(src = "ugplot.png", height = "50px"),"ugPlot"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file1", "Choose CSV File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      lapply(1:nrow(plotlist), function(i){
-        bname <- paste0("buttonplot",i)
-        imgname <- paste0("img/",plotlist$img[i])
-        print(imgname)
-        fluidRow(
-          tags$img(src = imgname, width = 130, height = 130),
-          actionButton(bname, plotlist$name[i])
-        )}),
+  tabsetPanel(
+    tabPanel("TABLE",
+             tags$div(
+               style = "display: inline-block; vertical-align: top;",
+               fileInput(
+                 "file1", "Choose a CSV file",
+                 multiple = FALSE,
+                 accept = c(
+                   "text/csv",
+                   "text/comma-separated-values,text/plain",
+                   ".csv"
+                 )
+               )
+             ),
+             tags$div(
+               style = "display: inline-block; vertical-align: top;",
+               actionButton("transpose_table", "Transpose table",
+                            style = "height: 100%;")
+             ),
+             div(style = "width: 100%; overflow-x: auto;",
+                 DTOutput("contents")
+             ),
+             column(width = 6,
+                    actionButton("uncheck_all_columns", "Uncheck all"),
+                    actionButton("check_all_columns", "Check all"),
+                    div(class = "scrollable-table",
+                        div(id = "dynamic_columns")
+                    )
+             ),
+             column(width = 6,
+                    actionButton("uncheck_all_rows", "Uncheck all"),
+                    actionButton("check_all_rows", "Check all"),
+                    div(class = "scrollable-table",
+                        div(id = "dynamic_rows")
+                    )
+             ),
     ),
-
-    mainPanel(
-      tabsetPanel(
-        tabPanel("TABLE",
-                 div(style = "width: 100%; overflow-x: auto;",
-                     DTOutput("contents")
-                 ),
-                 actionButton("transpose_table", "Transpose table"),
-                 actionButton("uncheck_all_columns", "Uncheck All Columns"),
-                 actionButton("check_all_columns", "Check All Columns"),
-                 actionButton("uncheck_all_rows", "Uncheck All Rows"),
-                 actionButton("check_all_rows", "Check All Rows"),
-                 column(width = 6,
-                        div(class = "scrollable-table",
-                            div(id = "dynamic_columns")
-                        )
-                 ),
-                 column(width = 6,
-                        div(class = "scrollable-table",
-                            div(id = "dynamic_rows")
-                        )
-                 ),
-        ),
-        tabPanel("PLOT",
+    tabPanel("PLOT",
+             sidebarLayout(
+               sidebarPanel(
+                 lapply(1:nrow(plotlist), function(i){
+                   bname <- paste0("buttonplot",i)
+                   imgname <- paste0("img/",plotlist$img[i])
+                   print(imgname)
+                   fluidRow(
+                     tags$img(src = imgname, width = 130, height = 130),
+                     actionButton(bname, plotlist$name[i])
+                   )}),
+               ),
+               mainPanel(
                  plotOutput("plot", height = "800px")
-        )
-      )
+               )
+             )
     )
   )
 )
 
 server <- function(input, output, session) {
   changed_table <<- ""
-  transpose_table <<- 0
+  transpose_table2 <<- reactiveVal(0)
+  refresh_counter <<- reactiveVal(0)
   file_click_count <<- reactiveVal(0) # Initialize the counter
   last_file_click_count <<- 0
   observeEvent(input$file1, {
@@ -71,41 +83,14 @@ server <- function(input, output, session) {
   })
 
   output$contents <- renderDT({
-    filepath <- req(input$file1$datapath)
-    filepath <- "sample_dataset.txt" ### temporary to speed up
-    df <<- read.csv2(filepath, header = TRUE, sep = "\t", row.names=1, dec=".", stringsAsFactors=FALSE, strip.white = TRUE)
-    print(paste("Transpose",transpose_table))
-    # if(transpose_table == 0){
-    #   changed_table <<- as.matrix(df)
-    # } else {
-    #   changed_table <<- t(as.matrix(df))
-    # }
-    changed_table <<- as.matrix(df)
-
     if(last_file_click_count == 0){
+      filepath <- req(input$file1$datapath)
+      filepath <- "sample_dataset.txt" ### temporary to speed up
+      df <<- read.csv2(filepath, header = TRUE, sep = "\t", row.names=1, dec=".", stringsAsFactors=FALSE, strip.white = TRUE)
+
+      changed_table <<- as.matrix(df)
       last_file_click_count <<- file_click_count()
-      insertUI(
-        selector = "#dynamic_columns",
-        where = "afterEnd",
-        ui = checkboxGroupInput(
-          inputId = "column_checkbox_group",
-          label = "Columns:",
-          choices = names(df),
-          selected = names(df)
-        )
-      )
-
-      insertUI(
-        selector = "#dynamic_rows",
-        where = "afterEnd",
-        ui = checkboxGroupInput(
-          inputId = "row_checkbox_group",
-          label = "Rows:",
-          choices = rownames(df),
-          selected = rownames(df)
-        )
-      )
-
+      load_checkbox_group()
     }
     return(changed_table[input$row_checkbox_group,input$column_checkbox_group])
   })
@@ -149,18 +134,43 @@ server <- function(input, output, session) {
     )
   })
   observeEvent(input$transpose_table, {
-    print("aqui transpose")
-    ntransposetable <- t(changed_table)
-    print(ntransposetable)
-    transpose_table <<- 1
 
-    #df <<- as.matrix()
-    #df <<- read.csv2(filepath, header = TRUE, sep = "\t", row.names=1, dec=".", stringsAsFactors=FALSE, strip.white = TRUE)
-    #changed_table <<- as.matrix(df)
-
-    # changed_table <<- ntransposetable
+    if(transpose_table2() == 0){
+      transpose_table2(1)
+    } else {
+      transpose_table2(0)
+    }
+    df <<- data.frame(t(as.matrix(df)))
+    changed_table <<- as.matrix(df)
+    load_checkbox_group()
   })
+}
 
+load_checkbox_group <- function() {
+  print("here")
+  removeUI(selector = paste0("#", "column_checkbox_group"))
+  removeUI(selector = paste0("#", "row_checkbox_group"))
+  insertUI(
+    selector = "#dynamic_columns",
+    where = "afterEnd",
+    ui = checkboxGroupInput(
+      inputId = "column_checkbox_group",
+      label = "Columns:",
+      choices = names(df),
+      selected = names(df)
+    )
+  )
+
+  insertUI(
+    selector = "#dynamic_rows",
+    where = "afterEnd",
+    ui = checkboxGroupInput(
+      inputId = "row_checkbox_group",
+      label = "Rows:",
+      choices = rownames(df),
+      selected = rownames(df)
+    )
+  )
 }
 
 shinyApp(ui, server)
